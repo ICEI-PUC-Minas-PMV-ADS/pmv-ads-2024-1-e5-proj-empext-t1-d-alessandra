2ff810e9d94fa498ee3adf9095c08aee2daca7e4
TorugoH <vitorhugoemail@gmail.com>
2024-03-04 20:27:57 -0300

    Adicionando estrutua inicial do back end

---
+++ codigo-fonte/back-end/dalessandra/src/main/java/back/end/dalessandra/DalessandraApplication.java
@@ -0,0 +1,13 @@
+package back.end.dalessandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalessandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalessandraApplication.class, args);
+	}
+
+}
---
+++ codigo-fonte/back-end/dalessandra/src/test/java/back/end/dalessandra/DalessandraApplicationTests.java
@@ -0,0 +1,13 @@
+package back.end.dalessandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalessandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
