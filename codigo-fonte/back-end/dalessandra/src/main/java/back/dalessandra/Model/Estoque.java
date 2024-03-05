cd5c696967114bb5f1f2007ecfa16353fa0b2ac7
TorugoH <vitorhugoemail@gmail.com>
2024-03-05 11:47:11 -0300

    Arrumando configuração do banco e adicionando tabela de estoque via ORM

---
+++ codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/Model/Estoque.java
@@ -0,0 +1,39 @@
+package back.dalessandra.Model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Estoque {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codProduto;
+    @Column(length=30, nullable = false)
+    private String nomePorduto;
+    @Column(nullable = false)
+    private int quantidadeItem;
+    @Column(length = 10)
+    private String marca;
+    @Column(length = 20)
+    private String tipo;
+    @Column(length = 20)
+    private String cor;
+    @Column(length = 5, nullable = false)
+    private  String tamanho;
+    @Column(nullable = false)
+    private float valorComprado;
+    @Column(nullable = false)
+    private  float valorVenda;
+    @Column(nullable = false)
+    private Date dataCadastro;
+
+}
