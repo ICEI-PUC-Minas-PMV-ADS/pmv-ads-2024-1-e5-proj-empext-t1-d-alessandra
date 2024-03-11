3628998a9e8b3ddea82ae3c5fdb57ef28845ca16
mthszf99 <101369477+mthszf99@users.noreply.github.com>
2024-03-11 19:14:57 -0300

    iniciando controller, repository e service do financeiro

---
+++ codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/controller/financeiro/FinanceiroController.java
@@ -0,0 +1,28 @@
+package back.dalessandra.controller.financeiro;
+
+import back.dalessandra.Model.Financeiro;
+import back.dalessandra.service.estoque.FinanceiroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RestController
+@RequestMapping("/financeiro")
+public class FinanceiroController {
+    @Autowired
+    FinanceiroService financeiroService;
+
+    @GetMapping()
+    public List<Financeiro> listarFinanceiro(){
+        return  financeiroService.listarFinanceiro();
+    }
+    @DeleteMapping("{id}")
+    public String excluirDespesa(@PathVariable("id") int id){
+        return financeiroService.excluirDespesa(id);
+    }
+
+}
