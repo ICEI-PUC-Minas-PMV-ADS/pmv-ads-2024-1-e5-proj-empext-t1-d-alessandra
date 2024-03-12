a9fa91043d3b4a33e0651e06a306e3f4bb211a88
Matheus Sampaio <matheuslemos98@gmail.com>
2024-03-11 21:26:04 -0300

    feat: realiza endpoints vendas

--- codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/Model/Venda.java
+++ codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/Model/Venda.java
@@ -1,19 +1,28 @@
 package back.dalessandra.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Venda {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer codVenda;
+    @Column
     private LocalDateTime dtVenda;
+    @Column
     private Integer codCliente;
+    @Column
     private String formaPagto;
+    @Column
     private Double vlTotal;
+
 }
--- codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/repository/venda/VendaRepository.java
+++ codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/repository/venda/VendaRepository.java
@@ -1,22 +1,30 @@
 package back.dalessandra.repository.venda;
 
 import back.dalessandra.Model.Venda;
-import lombok.RequiredArgsConstructor;
-import org.jooq.DSLContext;
-import org.springframework.stereotype.Repository;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-@Repository
-public class VendaRepository {
+public interface VendaRepository extends JpaRepository<Venda,Integer> {
 
-    private final DSLContext sql;
+    @Query("select u from Venda u")
+    List<Venda> findAll();
 
-    public VendaRepository(DSLContext sql) {
-        this.sql = sql;
-    }
+    @Modifying
+    @Transactional
+    @Query("insert into Venda (codVenda, codCliente, dtVenda, formaPagto, vlTotal) " +
+            "values (:#{#venda.codVenda}, " +
+            ":#{#venda.codCliente}, " +
+            ":#{#venda.dtVenda}, " +
+            ":#{#venda.formaPagto}, " +
+            ":#{#venda.vlTotal})")
+    void create(Venda venda);
 
-    public List<Venda> findAll() {
-        return sql.select().from("vendas").fetchInto(Venda.class);
-    }
+    @Modifying
+    @Transactional
+    @Query("delete from Venda where codVenda = :codVenda")
+    void deleteByVenda(Integer codVenda);
 }
