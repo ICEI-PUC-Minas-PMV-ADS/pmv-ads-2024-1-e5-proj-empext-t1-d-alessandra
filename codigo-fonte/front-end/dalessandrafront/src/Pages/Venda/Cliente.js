e73fa383745da4f420760c87fa819535fd048519
Matheus Sampaio <matheuslemos98@gmail.com>
2024-03-18 21:36:44 -0300

    feat: inicia front cadatro cliente

---
+++ codigo-fonte/front-end/dalessandrafront/src/Pages/Venda/Cliente.js
@@ -0,0 +1,213 @@
+
+import axios from "axios";
+import React from "react";
+import "../../Pages/estilo/estoque.css";
+import config from "../../config/config";
+import { useEffect, useState } from "react";
+import Card from "../../Componentes/Card/Card";
+import Menu from "../../Componentes/Menu/Menu";
+import LogoInvetario from "../../img/warehouse.png";
+import Tabela from "../../Componentes/Tabela/Tabela";
+import Filtro from "../../Componentes/Tabela/Filtro";
+import ModalAdicionar from "../../Componentes/Modal/ModiasEstoque/modalAdcionar";
+function Cliente(){
+
+    const [estoque, setEstoque] = useState([])
+    const [quantidadeEstoque, setQuantidadeEstoque] = useState(0)
+    const [valorEstoque, setValorEstoque] = useState(0)
+    const [filtro, setFiltro] = useState('');
+    useEffect(() => {
+        obeterEstoque()
+        obterQuantidadeItemEstoque()
+        obterValorEstoque()
+    },[])
+    const handleFiltroChange = (filtro) => {
+        setFiltro(filtro);
+    };
+    function obeterEstoque(){
+        const headers ={"Content-Type":"application/json"}
+        axios.get(config.URL+'estoque',{headers})
+             .then((response) => {
+                setEstoque(response.data)
+                })
+            .catch((error) => {
+                    console.log(error)
+                })
+     }
+     function obterQuantidadeItemEstoque(){
+        const headers ={"Content-Type":"application/json"}
+        axios.get(config.URL+'estoque/quantidadeDeItemCadastrados',{headers})
+             .then((response) => {
+                setQuantidadeEstoque(response.data)
+                })
+            .catch((error) => {
+                    console.log(error)
+                })
+     }
+     function obterValorEstoque(){
+        const headers ={"Content-Type":"application/json"}
+        axios.get(config.URL+'estoque/valorEstoqueComprado',{headers})
+             .then((response) => {
+                setValorEstoque(response.data)
+                })
+            .catch((error) => {
+                    console.log(error)
+                })
+     }
+    return(
+        <main className="bg-base-100 drawer lg:drawer-open" >
+            <Menu/>
+            <br></br>
+            <div className="drawer-content"> 
+            <section className="container mx-auto p-4 alinhamentoMenu2">
+                <img  class="h-10 w-10" fill="none" viewBox="0 0 34 34" src={LogoInvetario}/>
+                <h1 className="text-3xl font-bold">Novo Cliente</h1>
+            </section>
+            <br></br>
+            <br></br>
+            <section className="container mx-auto p-4 shadow-xl alinhamentoMenu2">
+                <ModalAdicionar/>
+                <Filtro onFiltroChange={handleFiltroChange}/>
+            </section>
+            <br></br>
+            <section className="container mx-auto p-4 shadow-xl overflow-x-auto" > 
+                <h3 className="text-2xl font-bold corTexto">Cadastro</h3>
+                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div className="sm:col-span-2">
+                        <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                        Primeiro Nome
+                        </label>
+                        <div className="mt-2">
+                        <input
+                            type="text"
+                            name="first-name"
+                            id="primeiroNome"
+                            autoComplete="given-name"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                        Ultimo nome
+                        </label>
+                        <div className="mt-2">
+                        <input
+                            type="text"
+                            name="first-name"
+                            id="ultimoNome"
+                            autoComplete="given-name"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        </div>
+                    </div>
+                    <div/>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Email
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Telefone
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="telefone"
+                                type="phone"
+                                autoComplete="phone"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div/>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            CPF/CNPJ
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="cpf"
+                                type="number"
+                                autoComplete="number"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div className="sm:col-span-4"/>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Rua
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="rua"
+                                type="text"
+                                autoComplete="text"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Bairro
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="bairro"
+                                type="text"
+                                autoComplete="text"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Cidade
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="cidade"
+                                type="text"
+                                autoComplete="text"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Complemento Endereço
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="complemento"
+                                type="text"
+                                autoComplete="text"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-900 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            </div>
+            
+        </main>
+    )
+
+}export default Cliente;
+
--- codigo-fonte/front-end/dalessandrafront/src/index.js
+++ codigo-fonte/front-end/dalessandrafront/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import Login from './Pages/Login/Login';
 import Estoque from './Pages/Estoque/Estoque';
 import Cadastro from './Pages/Cadastro/Cadastro';
+import Cliente from './Pages/Venda/Venda';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Estoque />
+  },
+  {
+    path: "/cliente",
+    element: <Cliente />
   },
 
 ]);
