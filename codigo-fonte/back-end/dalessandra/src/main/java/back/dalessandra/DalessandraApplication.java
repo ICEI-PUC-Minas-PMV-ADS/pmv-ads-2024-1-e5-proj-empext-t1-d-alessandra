ab7f555de7fb36db67aa4a205e99b55b502fc400
TorugoH <vitorhugoemail@gmail.com>
2024-03-26 19:51:01 -0300

    adicionando interface de envio de email

--- codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/DalessandraApplication.java
+++ codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/DalessandraApplication.java
@@ -1,13 +1,21 @@
 package back.dalessandra;
 
+import back.dalessandra.Model.Estoque;
+import back.dalessandra.repository.estoque.EstoqueRepository;
+import back.dalessandra.service.envioEmail.EmailEnvio;
+import back.dalessandra.service.estoque.EstoqueService;
+import back.dalessandra.service.estoque.EstoqueServiceMore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.util.*;
+import  java.util.List;
 @SpringBootApplication
 public class DalessandraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DalessandraApplication.class, args);
+
 	}
 
 }
--- codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/service/estoque/EstoqueService.java
+++ codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/service/estoque/EstoqueService.java
@@ -2,6 +2,8 @@ package back.dalessandra.service.estoque;
 
 import back.dalessandra.Model.Estoque;
 import back.dalessandra.repository.estoque.EstoqueRepository;
+import back.dalessandra.service.envioEmail.EmailEnvio;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +15,7 @@ import java.util.List;
 public class EstoqueService {
     @Autowired
     EstoqueRepository estoqueRepository;
-
+    EmailEnvio emaiEnvio =new EmailEnvio();
     public void cadastrarEstoque(Estoque estoque){
         int qtdAtual = estoque.getQuantidadeItem();
         float valorComprado = estoque.getValorComprado();
@@ -25,6 +27,11 @@ public class EstoqueService {
         estoqueRepository.save(estoque);
     }
 
+    @PostConstruct
+    public void init() {
+        eviarEmailNivelCriticoEstoque();
+    }
+
     public List<Estoque> listarEstoque(){
         return estoqueRepository.findAll();
     }
@@ -33,4 +40,50 @@ public class EstoqueService {
         estoqueRepository.deleteById(id);
         return ("Excluido com sucesso");
     }
+
+    public String eviarEmailNivelCriticoEstoque(){
+        List<Estoque> estoque = listarEstoque();
+        if(estoque != null){
+            StringBuilder htmlContent = new StringBuilder();
+            htmlContent.append("<html>" +
+                    "<head>" +
+                    "<title>Itens do Estoque</title>" +
+                    "</head>" +
+                    "<body>");
+            htmlContent.append("<h1>Itens do Estoque</h1>");
+            htmlContent.append(
+                    "<table>"
+                            + "<thead>"+
+                            "<tr>"+
+                            "<th>Codigo</th>"+
+                            "<th>Produto</th>"+
+                            "<th>Marca</th>"+
+                            "<th>Status</th>"+
+                            "<th>Cor</th>"+
+                            "<th>Quantidade</th>"+
+                            "</tr>"+
+                            "</thead>"
+            );
+            htmlContent.append("<tbody>");
+            for (Estoque item : estoque) {
+                htmlContent.append("<tr>");
+                htmlContent.append("<td>").append(item.getCodProduto()).append("</td>");
+                htmlContent.append("<td>").append(item.getNomeProduto()).append("</td>");
+                htmlContent.append("<td>").append(item.getMarca()).append("</td>");
+                htmlContent.append("<td>").append(item.getStatus()).append("</td>");
+                htmlContent.append("<td>").append(item.getCor()).append("</td>");
+                htmlContent.append("<td>").append(item.getQtdAtual()).append("</td>");
+                htmlContent.append("</tr>");
+            }
+            htmlContent.append("</tbody>");
+            htmlContent.append("</table>");
+            htmlContent.append("</body></html>");
+
+            emaiEnvio.sendEmail("vitorhugoemail@gmail.com","Listar Estoque",htmlContent.toString());
+            return "enviado";
+        }
+           return "Tudo ok";
+    }
+
+
 }
