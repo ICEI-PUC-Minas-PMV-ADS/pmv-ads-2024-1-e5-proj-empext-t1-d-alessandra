3f7b4b67efbaa39c57114d0c70e8f0f3194d0b76
Matheus Sampaio <matheuslemos98@gmail.com>
2024-05-23 19:41:38 -0300

    insere paginacao relatorio mais vendidos

--- codigo-fonte/front-end/dalessandrafront/src/Pages/Venda/RelatorioMaisVendidos.js
+++ codigo-fonte/front-end/dalessandrafront/src/Pages/Venda/RelatorioMaisVendidos.js
@@ -4,9 +4,12 @@ import config from "../../config/config";
 import Menu from "../../Componentes/Menu/Menu";
 import LogoVenda from "../../img/caixa-eletronico.png";
 import TabelaMaisVendidos from "./components/tabelaMaisVendidos";
+import { Paginator } from 'primereact/paginator';
 
 function MaisVendidos() {
     const [itens, setItens] = useState([]);
+    const [first, setFirst] = useState(0);
+    const [totalItens, setTotalItens] = useState(0);
 
     useEffect(() => {
         loadData();
@@ -16,9 +19,21 @@ function MaisVendidos() {
         const headers = { "Content-Type": "application/json" };
         try {
             const response = await axios.get(`${config.URL}item/mais-vendidos`, { headers });
-            setItens(response.data);
+            setItens(response.data.content);
         } catch (error) {
             console.error(error);
+        }
+    }
+
+    const onPageChange = async (event) => {
+        setFirst(event.first);
+        const headers ={"Content-Type":"application/json"}
+        try{
+            const response = await axios.get(`${config.URL}venda/mais-vendidos?page=${event.page}&size=${5}`, { headers });
+            setItens(response.data?.content);
+            setTotalItens(response.data?.totalElements);
+        } catch(error){
+            console.log(error)
         }
     }
     
@@ -31,40 +46,15 @@ function MaisVendidos() {
                     <img class="h-10 w-10" fill="none" viewBox="0 0 34 34" src={LogoVenda} alt="Logo Venda" />
                     <h1 className="text-3xl font-bold">Itens Mais Vendidos</h1>
                 </section>
-                <section className="container mx-auto p-4 shadow-xl overflow-x-auto" >
-                    <div className="drawer-content">
-                        <div >
-                            <input
-                                type="date"
-                                //value={filtroDataVenda}
-                                //onChange={changeDate}
-                                placeholder="Data da Compra"
-                                className="input input-bordered input-success w-full max-w-xs"
-                            />
-                        </div>
-                        <div></div>
-                    </div>
-                </section>
                 <br />
                 <section className="container mx-auto p-4 shadow-xl overflow-x-auto" >
-                    {/* <div className="flex justify-between items-center">
-                        <h3 className="text-2xl font-bold corTexto">Vendas do dia</h3>
-                        <div className="drawer-content">
-                            <input
-                                type="date"
-                                value={filtroDataVenda}
-                                onChange={changeDate}
-                                placeholder="Data da Compra"
-                                className="input input-bordered input-success w-full max-w-xs"
-                            />
-                        </div>
-                    </div> */}
-                    <br />
-                    {/* <div className="join">
-                        <button className="join-item btn" onClick={backPage}>«</button>
-                        <button className="join-item btn">Página {paginaAtual + 1}</button>
-                        <button className="join-item btn" onClick={nextPage}>»</button>
-                    </div> */}
+                    <Paginator 
+                        first={first} 
+                        rows={5} 
+                        totalRecords={totalItens} 
+                        onPageChange={onPageChange} 
+                        template={{ layout: 'PrevPageLink CurrentPageReport NextPageLink' }} 
+                    />
                     <br />
                     { <TabelaMaisVendidos dados={itens} /> }
                 </section>
