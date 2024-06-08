2b256b6542af17e99d0bf465bd58484a97c08733
Vitor Hugo <vitorhugoemail@gmail.com>
2024-06-08 11:04:23 -0300

    Update ConfigurandoServidorLocal.java

--- codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/ConfigurandoServidorLocal.java
+++ codigo-fonte/back-end/dalessandra/src/main/java/back/dalessandra/ConfigurandoServidorLocal.java
@@ -9,7 +9,7 @@ public class ConfigurandoServidorLocal implements WebMvcConfigurer {
     @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins("*")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
         }
     }
